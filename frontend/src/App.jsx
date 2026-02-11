5c4981cb5a49a7b3a4445b5acb065f8e3df85de1
Aradhana Yadav <yadavaradhana529@gmail.com>
2026-02-11 17:51:17 +0530

    app fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
 
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import Login from './components/Login'
+
 import Dashboard from './components/Dashboard'
 import Register from './components/register'
+import Login from './components/Login'
+
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Register/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
     </div>
